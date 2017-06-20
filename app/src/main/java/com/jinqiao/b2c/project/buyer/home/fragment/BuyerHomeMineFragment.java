0e1c7e60e84ad8d17bd8551ff0109faadda4df90
liulei <649444395@qq.com>
2017-06-20 18:10:50 +0800

    增加我的页面

--- app/src/main/java/com/jinqiao/b2c/project/buyer/home/fragment/BuyerHomeMineFragment.java
+++ app/src/main/java/com/jinqiao/b2c/project/buyer/home/fragment/BuyerHomeMineFragment.java
@@ -1,8 +1,15 @@
 package com.jinqiao.b2c.project.buyer.home.fragment;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+
 import com.jinqiao.b2c.R;
 import com.jinqiao.b2c.compent.base.BaseFragment;
 import com.jinqiao.b2c.compent.cdi.cmp.FragmentComponent;
+import com.jinqiao.b2c.compent.ui.HeadBar;
+
+import butterknife.Bind;
 
 /**
  * 用途：
@@ -11,7 +18,11 @@ import com.jinqiao.b2c.compent.cdi.cmp.FragmentComponent;
  */
 
 
-public class BuyerHomeMineFragment extends BaseFragment{
+public class BuyerHomeMineFragment extends BaseFragment {
+    @Nullable
+    @Bind(R.id.header_)
+    HeadBar mHeadBar;
+
     @Override
     public void doInject(FragmentComponent component) {
         component.plus(this);
@@ -20,5 +31,18 @@ public class BuyerHomeMineFragment extends BaseFragment{
     @Override
     protected int getRootViewId() {
         return R.layout.fragment_buyer_mine;
+    }
+
+    @Override
+    public void afterViewBind(Bundle saveInstanceState) {
+        super.afterViewBind(saveInstanceState);
+        mHeadBar.setTitle("我的");
+        mHeadBar.setBackVisable(false);
+        mHeadBar.addText("设置", new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
