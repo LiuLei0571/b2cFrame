d649d7c7f61a173020a49bc793a0de6e82f08c4d
liulei <649444395@qq.com>
2017-08-31 21:06:12 +0800

    成功植入沉浸式状态栏

--- app/src/main/java/com/jinqiao/b2c/project/buyer/home/fragment/BuyerHomeCarFragment.java
+++ app/src/main/java/com/jinqiao/b2c/project/buyer/home/fragment/BuyerHomeCarFragment.java
@@ -1,5 +1,8 @@
 package com.jinqiao.b2c.project.buyer.home.fragment;
 
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
 import com.jinqiao.b2c.R;
 import com.jinqiao.b2c.compent.base.BaseFragment;
 import com.jinqiao.b2c.compent.cdi.cmp.FragmentComponent;
@@ -12,6 +15,7 @@ import com.jinqiao.b2c.compent.cdi.cmp.FragmentComponent;
 
 
 public class BuyerHomeCarFragment extends BaseFragment {
+    Toolbar mToolbar;
     @Override
     public void doInject(FragmentComponent component) {
         component.plus(this);
@@ -20,5 +24,19 @@ public class BuyerHomeCarFragment extends BaseFragment {
     @Override
     protected int getRootViewId() {
         return R.layout.fragment_buyer_car;
+    }
+
+    @Override
+    public void beforeViewBind(View rootView) {
+        super.beforeViewBind(rootView);
+        mToolbar = (Toolbar) rootView.findViewById(R.id.mine_b2c_tool_bar);
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        super.initImmersionBar();
+        mImmersionBar.titleBar(mToolbar).statusBarDarkFont(true,0.2f).init();
     }
 }
